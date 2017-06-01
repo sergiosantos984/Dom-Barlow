adba3b3f2488e411ccf3ed3ccd720e6db266e45d
sergiosantos984 <sergiosantos984@gmail.com>
2017-06-01 15:55:08 +0100

    dom

---
+++ app.js
@@ -0,0 +1,5 @@
+$('body').css('display','none').fadeIn(1000);
+$('h1').css('display','none').fadeIn(1000);
+$('h2').css('display','none').fadeIn(1000);
+$('img').css('display','none').fadeIn(1500);
+$('p').css('display','none').fadeIn(2000);
